a93e2c2ae4766fea0e4d16fae03ac49f9dcc8a1e
kimdohui <kdh24162416@gmail.com>
2020-03-26 00:01:18 +0900

    컴포넌트 성능 최적화(useReducer사용)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useReducer } from 'react';
 import TodoTemplate from './TodoTemplate';
 import TodoInsert from './TodoInsert';
 import TodoList from './TodoList';
 
+function createBulkTodos() {
+  const array = [];
+  for (let i; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할일 ${i}`,
+      checked: false,
+    });
+  }
+  return array;
+}
+
+function todoReducer(todos, action) {
+  switch (action.type) {
+    //새로 추가함 { type: 'INSERT' , todo: {id:1 , text : 'todo', checked: false }}
+    case 'INSERT':
+      return todos.concat(action.todo);
+    case 'REMOVE':
+      return todos.filter(todo => todo.id !== action.id);
+    //제거함 {type: 'REMOVE', id:1 }
+    case 'TOGLE':
+      return todos.map(todo =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,
+      );
+    //토글
+    default:
+      return todos;
+  }
+}
+
 const App = () => {
-  const [todos, setTodos] = useState([]);
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
 
   //고윳값으로 사용될 id
   //ref를 사용해 변수 담기
-  const nextId = useRef(4);
+  const nextId = useRef(2501);
 
   //추가
   const onInsert = useCallback(text => {
@@ -17,22 +47,18 @@ const App = () => {
       text,
       checked: false,
     };
-    setTodos(todos => todos.concat(todo));
+    dispatch({ type: 'INSERT', todo });
     nextId.current += 1;
   }, []);
 
   //삭제
   const onRemove = useCallback(id => {
-    setTodos(todos => todos.filter(todo => todo.id !== id));
+    dispatch({ type: 'REMOVE', id });
   }, []);
 
   //수정
   const onToggle = useCallback(id => {
-    setTodos(todos =>
-      todos.map(todo =>
-        todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-      ),
-    );
+    dispatch({ type: 'TOGGLE', id });
   }, []);
   return (
     <TodoTemplate>
